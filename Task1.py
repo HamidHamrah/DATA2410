173a744ab06ea947ff64a75fe2879c72091b8b14
Hamid Hamrah <89603079+HamidHamrah@users.noreply.github.com>
2023-02-02 13:28:23 +0100

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,4 +1,4 @@
-def jain(num1,num2):            # Vi at definding a function here which is called jain
+def jain(num1,num2):            # we are defining a function here which is called jain
     nummerator=(num1+num2)**2# To find the JFI we shoul find the nummerator 
     denominator=(num1**2+num2**2)   # After that vi find the denominator w
     JFI=nummerator/denominator  # As from the fomul given we know that the nummerator should be deived to denominator and we will get the JFI
